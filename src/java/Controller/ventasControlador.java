e3d42d8fc8df973cd68bf75854d5c20b09d1a3f6
Mauricio <Mauricio@DESKTOP-ORSGVTP>
2017-01-16 13:52:39 -0500

    SEGUNDA SUBIDA DEL REPOSITORIO, IMPLEMENTACION DE LA BASE DE DATOS

---
+++ src/java/Controller/ventasControlador.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Mauricio
+ */
+@Named(value = "ventasControlador")
+@Dependent
+public class ventasControlador {
+
+    /**
+     * Creates a new instance of ventasControlador
+     */
+    public ventasControlador() {
+    }
+    
+}
